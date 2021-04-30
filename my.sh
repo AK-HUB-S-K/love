b695ba321b16a1c96007e93912e1784bf3513868
AK HUB <83388700+AK-HUB-S@users.noreply.github.com>
2021-04-30 20:43:54 +0530

    Create my.sh

---
+++ my.sh
@@ -0,0 +1,31 @@
+#!/data/data/com.termux/files/usr/bin/bash
+######################################
+#        PROJECT: Tmatrix            #
+#        Author: AJAY O S            #
+######################################
+echo -e "\e[32m[*]\e[34m WELCOME TO AK HUB TOOLS  \e[m "
+echo
+#Installing the following packages to install Tmatrix
+echo " "
+echo -e "\e[32m[*]\e[34m Installing Tmatrix \e[m "
+echo " "
+echo " "
+echo -e  "\e[32m do you want to install it(y/n) \e[m"
+read inan
+sleep 3
+
+if [ $inan = y ];
+then 
+echo -e "\e[31m[*]\e[32mYou are hacked... \e[m"
+sleep 2
+bash main.sh
+echo " "
+elif [ $inan = n ];
+then
+   echo 'ok'
+cd $HOME
+else
+then 
+   echo 'type y/n'
+fi
+
