7e4577f8cd699decac7b5b138b01d5b0d7203c81
AK HUB <83388700+AK-HUB-S@users.noreply.github.com>
2021-05-01 12:42:13 +0530

    Update love.sh

--- love.sh
+++ love.sh
@@ -80,19 +80,22 @@ sleep 2.0
 clear
 
 
-if [ "$answer" = "{answer#[Yy]}" ] ;then 
+if [ "$answer" = "${answer#[Yy]}" ] ;
+then 
 tput cup 12 22
     echo -e "\e[42mI LOVE YOU \e[m"
 sleep 2
 bash iloveyou.sh
-elif [  "$answer" = "{answer#[Nn]}" ];then
+elif [  "$answer" = "${answer#[Nn]}" ];
+then
 tput cup 17 23
    echo -e "\e[42mBUT I LOVE YOU \e[m"
 tput cup 17 23
    echo -e "\e[42m YOU ARE HACKED BY ME \e[m"
 sleep 2
 bash hate.sh 
-elif [  "$answer" != "{answer#[YyNn]}" ];then
+elif [  "$answer" != "${answer#[YyNn]}" ];
+then
 tput cup 17 21
    echo -e "\e[42m use y/n to answer me \e[m"
    echo " "
