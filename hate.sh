e5fdc52a617f047b81bbc30d43d11b1370b89e65
AK HUB <83388700+AK-HUB-S@users.noreply.github.com>
2021-05-01 12:55:53 +0530

    Create hate.sh

---
+++ hate.sh
@@ -0,0 +1,210 @@
+#!/data/data/com.termux/files/user
+clear 
+
+#installing cmatrix
+apt-get install cmatrix -y
+
+echo "toilet -f slant YOU ARE HACKED -F gay | lolcat"
+sleep 3
+echo " "
+echo " "
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m " 
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m " 
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m " 
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m " 
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m " 
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m " 
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 2 
+echo -e "\e[46mYOU ARE HACKED \e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED\e[m \e[36m YOU ARE HACKED \e[m \e[45m YOU ARE HACKED \e[m "  
+sleep 4
+#spinner 
+bash load.sh
+sleep 3.0
+clear 
+
+cmatrix 
