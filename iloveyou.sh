d58dc700a30244470c44cb5a010ecfbbb7828b63
AK HUB <83388700+AK-HUB-S@users.noreply.github.com>
2021-05-01 11:29:43 +0530

    Add files via upload

---
+++ iloveyou.sh
@@ -0,0 +1,27 @@
+#!/data/data/com.termux/files/usr/bin/bash
+clear
+
+echo "toilet -f slant  I LOVE YOU  -F gay | lolcat"
+sleep 2
+
+toilet I LOVE YOU 
+sleep 2
+clear
+echo " "
+echo -e "\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m " 
+echo -e "\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m " 
+echo -e "\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m " 
+echo -e "\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m " 
+echo -e "\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m " 
+echo -e "\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m " 
+echo -e "\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m " 
+echo -e "\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m " 
+echo -e "\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m " 
+echo -e "\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m\e[36m I LOVE YOU \e[m " 
+sleep 5 
+
+clear 
+
+cd $HOME 
+
+exit
